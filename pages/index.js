8a9d831c074241fbeda91bf1a45c2d2b14c81ade
JediWattzon <robertjd333@gmail.com>
2021-07-21 20:17:22 -0600

    setting up webpack

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
+import Image from 'next/image'
+
 function HomePage() {
-  return <div>Welcome to Next.js!</div>
+  return <Image src="/homer.jpeg" alt="homer" width="200" height="100" />
 }
 
 export default HomePage
---
+++ ship/homer/homer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Homer = () => {
+  return <img
+    src="/homer.jpeg"
+    alt="Homer Woohooo"
+  >
+}
